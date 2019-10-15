ff4caa1c0add2a76c21556076d61ab7c9d7db48d
PabloCalandria <pablocalandria22@gmail.com>
2019-10-15 18:03:34 -0300

    Se actualizo el route

--- route.php
+++ route.php
@@ -1,75 +1,20 @@
 <?php
+    // require_once importa los archivos que vamos a utilizar
 
-class Route {
-    private $url;
-    private $verb;
-    private $controller;
-    private $method;
-    private $params;
-
-    public function __construct($url, $verb, $controller, $method){
-        $this->url = $url;
-        $this->verb = $verb;
-        $this->controller = $controller;
-        $this->method = $method;
-        $this->params = [];
-    }
-    public function match($url, $verb) {
-        if($this->verb != $verb){
-            return false;
-        }
-        $partsURL = explode("/", trim($url,'/'));
-        $partsRoute = explode("/", trim($this->url,'/'));
-        if(count($partsRoute) != count($partsURL)){
-            return false;
-        }
-        foreach ($partsRoute as $key => $part) {
-            if($part[0] != ":"){
-                if($part != $partsURL[$key])
-                return false;
-            } //es un parametro
-            else
-            $this->params[$part] = $partsURL[$key];
-        }
-        return true;
-    }
-    public function run(){
-        $controller = $this->controller;  
-        $method = $this->method;
-        $params = $this->params;
-       
-        (new $controller())->$method($params);
-    }
-}
-
-class Router {
-    private $routeTable = [];
-    private $defaultRoute;
-
-    public function __construct() {
-        $this->defaultRoute = null;
-    }
-
-    public function route($url, $verb) {
-        //$ruta->url //no compila!
-        foreach ($this->routeTable as $route) {
-            if($route->match($url, $verb)){
-                //TODO: ejecutar el controller//ejecutar el controller
-                // pasarle los parametros
-                $route->run();
-                return;
-            }
-        }
-        //Si ninguna ruta coincide con el pedido y se configuró ruta por defecto.
-        if ($this->defaultRoute != null)
-            $this->defaultRoute->run();
-    }
+    require_once "controller/homeController.php";
     
-    public function addRoute ($url, $verb, $controller, $method) {
-        $this->routeTable[] = new Route($url, $verb, $controller, $method);
-    }
+    $partesURL = explode("/", $_GET["action"]);
 
-    public function setDefaultRoute($controller, $method) {
-        $this->defaultRoute = new Route("", "", $controller, $method);
+    if ($partesURL[0] == ""){
+        homeView();   //sino viene ninguna accion, inicia la funcion del archivo about.php
     }
-}
+    else{
+        if ($partesURL[0] == "agregar"){
+            insertDatos();   //sino viene ninguna accion, inicia la funcion del archivo about.php
+        }elseif($partesURL[0] == "borrar"){
+            borrarTarea($partesURL[1]);
+        }elseif($partesURL[0] == "actualiza"){
+            actualizaFecha($partesURL[1]);
+        }
+    }
+?>
