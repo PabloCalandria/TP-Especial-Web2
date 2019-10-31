934c8cdfbec42154d7fb13aa65c6cb211b71bf81
PabloCalandria <pablocalandria22@gmail.com>
2019-10-31 13:09:26 -0300

    merge controller contectus

--- controller/contactUsController.php
+++ controller/contactUsController.php
@@ -1,14 +1,15 @@
 <?php
 
 require_once "./view/contactUsView.php";
+include_once('./helpers/authHelper.php');
 
 class ContactUsController{
     
     private $view;
 
     function __construct(){
-        //$authHelper = new AuthHelper();
-        //$authHelper->checkLoggedIn();
+        $authHelper = new AuthHelper();
+        $authHelper->checkLoggedIn();
         $this->view = new ContactUsView();
     }
     
