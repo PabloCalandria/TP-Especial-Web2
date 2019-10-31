fe965b586f211b5eb3a6232297cc4fb295305f06
SSAAMMAA <facundo.samartino@gmail.com>
2019-10-31 20:27:55 -0300

    rollback a version estable

--- view/productsView.php
+++ view/productsView.php
@@ -9,17 +9,21 @@
             $this->smarty = new Smarty();
         }
         
-        function mostrar($lista, $style){
+        function mostrar($lista, $style, $admin){
             $this->smarty->assign('Context', explode("/", $_GET["action"]));
             $this->smarty->assign('Login', isset($_SESSION));
+            $this->smarty->assign('Admin', $_SESSION['ADMIN']);
+            $this->smarty->assign('admin', $admin);
             $this->smarty->assign('Lista', $lista);
             $this->smarty->assign('Estilos', $style);
             $this->smarty->display('templates/products.tpl');
         }
 
-        function mostrarProducto($product){
+        function mostrarProducto($product, $admin){
             $this->smarty->assign('Context', explode("/", $_GET["action"]));
             $this->smarty->assign('Login', isset($_SESSION));
+            $this->smarty->assign('Admin', $_SESSION['ADMIN']);
+            $this->smarty->assign('admin', $admin);
             $this->smarty->assign('Producto', $product);
             $this->smarty->display('templates/infoProduct.tpl');
         }
